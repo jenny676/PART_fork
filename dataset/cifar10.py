53290191a20e4e63d175680f93a1b77afe22597b
jenny676 <jenny.ep.je@gmail.com>
2025-12-19 15:02:09 +0000

    Update cifar10.py

--- dataset/cifar10.py
+++ dataset/cifar10.py
@@ -2,6 +2,9 @@ from torchvision.datasets import CIFAR10 as DATA
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+CIFAR_MEAN = [0.4914, 0.4822, 0.4465]
+CIFAR_STD  = [0.2023, 0.1994, 0.2010]
+
 class CIFAR10():
     def __init__(self, train_batch_size: int = 512, test_batch_size: int = 100, path: str = './data/'):
         self.train_batch_size = train_batch_size
@@ -13,11 +16,13 @@ class CIFAR10():
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
+        transforms.Normalize(mean=CIFAR_MEAN, std=CIFAR_STD),
         ])
 
     def transform_test(self):
         return transforms.Compose([
         transforms.ToTensor(),
+        transforms.Normalize(mean=CIFAR_MEAN, std=CIFAR_STD),
         ])
 
     def train_data(self):
