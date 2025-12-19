6b326f3036202d414a82aa8a8363d860e1ba66ed
jenny676 <jenny.ep.je@gmail.com>
2025-12-19 23:25:48 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,25 +11,6 @@ from loss.trades import *
 from loss.part_mart import *
 from loss.part_trades import *
 
-# --- add to utils.py (near other imports/constants) ---
-import torch
-
-CIFAR_MEAN = torch.tensor([0.4914, 0.4822, 0.4465])
-CIFAR_STD  = torch.tensor([0.2023, 0.1994, 0.2010])
-
-def denormalize(x: torch.Tensor, device: torch.device):
-    """Convert normalized tensor (model input space) -> pixel space [0,1]."""
-    mean = CIFAR_MEAN.to(device).view(1, 3, 1, 1)
-    std  = CIFAR_STD.to(device).view(1, 3, 1, 1)
-    return x * std + mean
-
-def renormalize(x: torch.Tensor, device: torch.device):
-    """Convert pixel-space tensor [0,1] -> normalized model input space."""
-    mean = CIFAR_MEAN.to(device).view(1, 3, 1, 1)
-    std  = CIFAR_STD.to(device).view(1, 3, 1, 1)
-    return (x - mean) / std
-# --- end paste ---
-
 
 def parse_fraction(fraction_string):
     if '/' in fraction_string:
@@ -232,5 +213,6 @@ def save_cam(model, train_loader, device, args):
         weighted_eps = generate_weighted_eps(weight_matrix, args)
         weighted_eps_list.append(weighted_eps)
     return weighted_eps_list
+
 
 
